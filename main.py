e650839860abafab49164df8b30bf4ff603d69fd
David <100460314@alumnos.uc3m.es>
2023-05-04 12:43:06 +0200

    no soy capaz de acabarlo luego lo vemos

--- main.py
+++ main.py
@@ -6,59 +6,91 @@ def main():
     data = json.load(fp)
     fp.close()
 
+    #Format for printing
+    key_format = "\033[1;35m"
+    value_format = "\033[1;36m"
+    separator_format = "\033[1;37m"
+
+
     # temperature = int(temperature)
     cost_on = int(data["costs"]["on"])
     cost_off = int(data["costs"]["off"])
     states_enumeration = data["states"]
     print(states_enumeration)
     previous_values = {}
+    new_values = {}
     # we store values with margins so that we do not have problems when we iterate in state 16, 24.5 and 25
     count = 15
     while count < 26:
         count = count + 0.5
         previous_values[count] = 0
+    
+    count = 15
+    while count < 26:
+        count = count + 0.5
+        new_values[count] = 0
 
-    print(previous_values)
+    #print(previous_values)
     count = 15.5
+    iteration=0
+    while count > 0:
+        if ((new_values[24] - previous_values[24]) > 0.0001) or iteration<1:
+            print(key_format, 24, separator_format, "->", value_format, previous_values[24])
+            print("-----------------------------------------")
+            print(key_format, 24, separator_format, "->", value_format, new_values[24])
+            print("00000000000000000000000000000000000000000")
 
-    while count < 25:
+            iteration += 1
+            count = 15
+            while count < 26:
+                count = count + 0.5
+                previous_values[count] = new_values[count]
+            #for key in previous_values:
 
-        count += 0.5
+            
+            count = 15.5
+            while count < 25:
+                count += 0.5
 
-        state_type = "State"
-        if count == 16:
-            state_type = "State16"
-        elif count == 24.5:
-            state_type = "State24,5"
-        elif count == 25:
-            state_type = "State25"
+                state_type = "State"
+                if count == 16:
+                    state_type = "State16"
+                elif count == 24.5:
+                    state_type = "State24,5"
+                elif count == 25:
+                    state_type = "State25"
 
-        summation_on = cost_on
-        action = "on"
-        for state_to_go in states_enumeration[state_type][action]:
-            # 1+float(state_to_go)  important change to float if you want to add to the count
-            summation_on += (float(states_enumeration[state_type][action][state_to_go]) * previous_values[
-                count + float(state_to_go)])
+                summation_on = cost_on
+                action = "on"
+                for state_to_go in states_enumeration[state_type][action]:
+                    # 1+float(state_to_go)  important change to float if you want to add to the count
+                    summation_on += (float(states_enumeration[state_type][action][state_to_go]) * previous_values[
+                        count + float(state_to_go)])
 
-        summation_off = cost_off
-        action = "off"
-        for state_to_go in states_enumeration[state_type][action]:
-            # 1+float(state_to_go)  important change to float if you want to add to the count
-            summation_off += (float(states_enumeration[state_type][action][state_to_go]) * previous_values[
-                count + float(state_to_go)])
+                summation_off = cost_off
+                action = "off"
+                for state_to_go in states_enumeration[state_type][action]:
+                    # 1+float(state_to_go)  important change to float if you want to add to the count
+                    summation_off += (float(states_enumeration[state_type][action][state_to_go]) * previous_values[
+                        count + float(state_to_go)])
 
-        previous_values[count] = min(summation_on, summation_off)
+                new_values[count] = min(summation_on, summation_off)
+        else:
+            print(key_format, 20, separator_format, "->", value_format, previous_values[20])
+            print(key_format, 20, separator_format, "->", value_format, new_values[20])
+            break
+        
+
 
     # print(previous_values)
 
-    key_format = "\033[1;35m"
-    value_format = "\033[1;36m"
-    separator_format = "\033[1;37m"
-    for key in previous_values:
-        print(key_format, key, separator_format, "->", value_format, previous_values[key])
 
+    #for key in previous_values:
+    """ print(key_format, 20, separator_format, "->", value_format, previous_values[20])
+    #for key in previous_values:
+    print(key_format, 20, separator_format, "->", value_format, new_values[20])"""
+    print(iteration)
     return None
-
 
 if __name__ == "__main__":
     main()
