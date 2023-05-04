3b03f1148069d56d68a43ee9cace5e5e8e9c06be
isalamajor <100472315@alumnos.uc3m.es>
2023-05-04 10:46:45 +0200

    lo mismo pero mas corto <3

--- main.py
+++ main.py
@@ -1,98 +1,65 @@
 import json
-import sys
+
 
 def main():
     fp = open('Values.json', 'r')
     data = json.load(fp)
     fp.close()
-    
-    
-    #temperature=int(temperature)
-    cost_on=int(data["costs"]["on"])
-    cost_off=int(data["costs"]["off"])
-    states_enumertion=data["states"]
-    previous_values={}
-    # we store values with margins so we dont have problems when we iterate in state 16, 24.5 and 25
+
+    # temperature = int(temperature)
+    cost_on = int(data["costs"]["on"])
+    cost_off = int(data["costs"]["off"])
+    states_enumeration = data["states"]
+    print(states_enumeration)
+    previous_values = {}
+    # we store values with margins so that we do not have problems when we iterate in state 16, 24.5 and 25
     count = 15
     while count < 26:
-        count=count + 0.5
+        count = count + 0.5
         previous_values[count] = 0
 
     print(previous_values)
     count = 15.5
+    state_type = "State"
+
     while count < 25:
-        
-        count = count + 0.5
-        
+
+        count += 0.5
+
+        state_type == "State"
         if count == 16:
-        
-            sumatory_on=cost_on
-            
-            for state_to_go in states_enumertion["State16"]["on"]:
-                
-                #1+float(state_to_go)  important change to float if you want to add to the count
-                sumatory_on = sumatory_on + (float(states_enumertion["State16"]["on"][state_to_go])*previous_values[count+float(state_to_go)])
-            
-            sumatory_off=cost_off   
-            for state_to_go in states_enumertion["State16"]["off"]:
-                #1+float(state_to_go)  important change to float if you want to add to the count
-                sumatory_off = sumatory_off + (float(states_enumertion["State16"]["off"][state_to_go])*previous_values[count+float(state_to_go)])
-           
-            previous_values[count] = min(sumatory_on,sumatory_off)
-
+            state_type == "State16"
         elif count == 24.5:
-            
-            sumatory_on=cost_on
-            for state_to_go in states_enumertion["State24,5"]["on"]:
-                
-                #1+float(state_to_go)  important change to float if you want to add to the count
-                sumatory_on = sumatory_on + (float(states_enumertion["State24,5"]["on"][state_to_go])*previous_values[count+float(state_to_go)])
-            
-            sumatory_off=cost_off   
-            for state_to_go in states_enumertion["State24,5"]["off"]:
-                #1+float(state_to_go)  important change to float if you want to add to the count
-                sumatory_off = sumatory_off + (float(states_enumertion["State24,5"]["off"][state_to_go])*previous_values[count+float(state_to_go)])
-           
-            previous_values[count] = min(sumatory_on,sumatory_off)
+            state_type == "State24,5"
         elif count == 25:
+            state_type == "State25"
 
-            sumatory_on=cost_on
-            for state_to_go in states_enumertion["State25"]["on"]:
-                
-                #1+float(state_to_go)  important change to float if you want to add to the count
-                sumatory_on = sumatory_on + (float(states_enumertion["State25"]["on"][state_to_go])*previous_values[count+float(state_to_go)])
-            
-            sumatory_off=cost_off   
-            for state_to_go in states_enumertion["State25"]["off"]:
-                #1+float(state_to_go)  important change to float if you want to add to the count
-                sumatory_off = sumatory_off + (float(states_enumertion["State25"]["off"][state_to_go])*previous_values[count+float(state_to_go)])
-           
-            previous_values[count] = min(sumatory_on,sumatory_off)
-        else:
-            
-            sumatory_on=cost_on
-            for state_to_go in states_enumertion["State"]["on"]:
-                
-                #1+float(state_to_go)  important change to float if you want to add to the count
-                sumatory_on = sumatory_on + (float(states_enumertion["State"]["on"][state_to_go])*previous_values[count+float(state_to_go)])
-            
-            sumatory_off=cost_off   
-            for state_to_go in states_enumertion["State"]["off"]:
-                #1+float(state_to_go)  important change to float if you want to add to the count
-                sumatory_off = sumatory_off + (float(states_enumertion["State"]["off"][state_to_go])*previous_values[count+float(state_to_go)])
-           
-            previous_values[count] = min(sumatory_on,sumatory_off)
-        
+        summation_on = cost_on
+        action = "on"
+        for state_to_go in states_enumeration[state_type][action]:
+            # 1+float(state_to_go)  important change to float if you want to add to the count
+            summation_on += (float(states_enumeration[state_type][action][state_to_go]) * previous_values[
+                count + float(state_to_go)])
 
-    
-    print(previous_values)
+        summation_off = cost_off
+        action = "off"
+        for state_to_go in states_enumeration[state_type][action]:
+            # 1+float(state_to_go)  important change to float if you want to add to the count
+            summation_off += (float(states_enumeration[state_type][action][state_to_go]) * previous_values[
+                count + float(state_to_go)])
+
+        previous_values[count] = min(summation_on, summation_off)
+
+    # print(previous_values)
+
+    key_format = "\033[1;35m"
+    value_format = "\033[1;36m"
+    separator_format = "\033[1;37m"
+    for key in previous_values.keys():
+        print(key_format, key, separator_format, "->", value_format, previous_values[key])
+
     return None
-    
-
-
-
-
 
 
 if __name__ == "__main__":
-   main()
+    main()
